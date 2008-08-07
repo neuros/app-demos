1dc60399df3abd6f03ca85bc01c3ec33cef7fc8d
Jorge Zapata <jzapata@neurostechnology.com>
2008-08-07 12:44:30 +0200

    Add 1080i mode to the tester and disable vid1 plane on every HD mode

--- fb/fbhelper.sh
+++ fb/fbhelper.sh
@@ -14,31 +14,48 @@ M720P_H=720
 M720P_W=1280
 M720p_MODE="U:1280x720p-30"
 
+M1080I_H=1080
+M1080I_W=1920
+M1080i_MODE="U:1920x1080i-30"
+
 
 if [ "$1" == "mode" ] ; then
 	if [ "$2" == "pal" ] ; then
 		W=$PAL_W
 		H=$PAL_H
 		MODE=$PAL_MODE
+		HD=0
 	elif [ "$2" == "ntsc" ] ; then
 		W=$NTSC_W
 		H=$NTSC_H
 		MODE=$NTSC_MODE
+		HD=0
+	elif [ "$2" == "1080i" ] ; then
+		W=$M1080I_W
+		H=$M1080I_H
+		MODE=$M1080i_MODE
+		HD=1
 	elif [ "$2" == "720p" ] ; then
 		W=$M720P_W
 		H=$M720P_H
 		MODE=$M720p_MODE
+		HD=1
 	elif [ "$2" == "480p" ] ; then
 		W=$M480P_W
 		H=$M480P_H
 		MODE=$M480p_MODE
+		HD=1
 	else
-		echo "Mode \"$2\" not supported. Valid modes are ntsc,pal,480p,720p"
+		echo "Mode \"$2\" not supported. Valid modes are ntsc,pal,480p,720p,1080i"
 		exit 1
 	fi
 
 	# set all slave planes to the right size, the switch master plane using sysfs
 	for plane in osd1 osd0 vid1 ; do fb-demo $plane output $W $H ; done
+	# in hd mode we should disable the vid1 plane
+	if [ $HD -eq 1 ]; then
+		fb-demo vid1 enable 0
+	fi
 	echo $MODE > /sys/class/graphics/fb1/mode
 
 elif [ "$1" == "plane" ] ; then

bbd2e0b6e2a9b476498f1ab11deee4d9ecda9d6f
Michael Gao <mgao@swoag.com>
2009-03-09 21:45:44 -0500

    VIDEO-DEMO: brought in resizer (DMA) support (in dirty ugly way).
        - lowers ARM overhead down to nearly zero.
    
    	modified:   video/main.c

--- video/main.c
+++ video/main.c
@@ -1,3 +1,218 @@
+// UGLY HACK to bring in resizer support to remove the CPU memcpy overhead
+#define RESZ_HACK
+
+#ifdef RESZ_HACK
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define ALWAYSERR
+
+#include <asm/arch/davinci_resizer.h>
+#include "rszcopy.h"
+
+/* Enables or disables debug output */
+#ifdef __DEBUG 
+#define DBG(fmt, args...) fprintf(stderr, "Rszcopy Debug: " fmt, ## args)
+#else
+#define DBG(fmt, args...)
+#endif
+
+#if (defined __DEBUG) || (defined ALWAYSERR)
+#define ERR(fmt, args...) fprintf(stderr, "Rszcopy Error: " fmt, ## args)
+#else
+#define ERR(fmt, args...)
+#endif
+
+#define RESIZER_DEVICE "/dev/davinci_resizer"
+#define FIR_RND_SCALE  256
+#define NUM_COEFS      32
+#define SCREEN_BPP     16
+
+static Rszcopy_Handle hRszcopy = RSZCOPY_FAILURE;
+
+/******************************************************************************
+ * inSizeCalc
+ ******************************************************************************/
+static int inSizeCalc(int inSize)
+{
+    int rsz;
+    int in = inSize;
+
+    while (1) {
+        rsz = ((in - 4) * 256 - 16) / (inSize - 1);
+
+        if (rsz < 256) {
+            if (in < inSize) {
+                break;
+            }
+            in++;
+        }
+        else if (rsz > 256) {
+            if (in > inSize) {
+                break;
+            }
+            in--;
+        }
+        else {
+            break;
+        }
+    }
+
+    return in;
+}
+
+/******************************************************************************
+ * Rszcopy_config
+ ******************************************************************************/
+int Rszcopy_config(Rszcopy_Handle hRszcopy,
+                   int width, int height, int srcPitch, int dstPitch)
+{
+    rsz_params_t  params = {
+        0,                              /* in_hsize (set at run time) */
+        0,                              /* in_vsize (set at run time) */
+        0,                              /* in_pitch (set at run time) */
+        RSZ_INTYPE_YCBCR422_16BIT,      /* inptyp */
+        0,                              /* vert_starting_pixel */
+        0,                              /* horz_starting_pixel */
+        0,                              /* cbilin */
+        RSZ_PIX_FMT_UYVY,               /* pix_fmt */
+        0,                              /* out_hsize (set at run time) */
+        0,                              /* out_vsize (set at run time) */
+        0,                              /* out_pitch (set at run time) */
+        0,                              /* hstph */
+        0,                              /* vstph */
+        {                               /* hfilt_coeffs */
+            256, 0, 0, 0, 256, 0, 0, 0, 256, 0, 0, 0, 256, 0, 0, 0,
+            256, 0, 0, 0, 256, 0, 0, 0, 256, 0, 0, 0, 256, 0, 0, 0
+        },
+        {                               /* vfilt_coeffs */
+            256, 0, 0, 0, 256, 0, 0, 0, 256, 0, 0, 0, 256, 0, 0, 0,
+            256, 0, 0, 0, 256, 0, 0, 0, 256, 0, 0, 0, 256, 0, 0, 0
+        },
+        {                               /* yenh_params */
+            RSZ_YENH_DISABLE,               /* type */
+            0,                              /* gain */
+            0,                              /* slop */
+            0                               /* core */
+        }
+    };
+
+    DBG("Configuring resizer job to copy image of resolution %dx%d\n",
+        width, height);
+
+    /* Set up the rszcopyr job */
+    params.in_hsize  = inSizeCalc(width);
+    params.in_vsize  = inSizeCalc(height);
+    params.in_pitch  = srcPitch;
+    params.out_hsize = width;
+    params.out_vsize = height;
+    params.out_pitch = dstPitch;
+
+    hRszcopy->inSize = srcPitch * params.in_vsize;
+    hRszcopy->outSize = dstPitch * params.out_vsize;
+
+    if (ioctl(hRszcopy->rszFd, RSZ_S_PARAM, &params) == -1) {
+        ERR("Rszcopy setting parameters failed.\n");
+        return RSZCOPY_FAILURE;
+    }
+
+    return RSZCOPY_SUCCESS;
+}
+
+/******************************************************************************
+ * Rszcopy_create
+ ******************************************************************************/
+Rszcopy_Handle Rszcopy_create(int rszSpeed)
+{
+    Rszcopy_Handle hRszcopy;
+
+    /* Allocate the rszcopy object */
+    hRszcopy = malloc(sizeof(Rszcopy_Object));
+
+    if (hRszcopy == NULL) {
+        ERR("Failed to allocate memory space for handle.\n");
+        return RSZCOPY_FAILURE;
+    }
+
+    /* Open rszcopyr device */
+    hRszcopy->rszFd = open(RESIZER_DEVICE, O_RDWR);
+
+    if (hRszcopy->rszFd == -1) {
+        ERR("Failed to open %s\n", RESIZER_DEVICE);
+        free(hRszcopy);
+        return RSZCOPY_FAILURE;
+    }
+
+    if (rszSpeed >= 0) {
+        if (ioctl(hRszcopy->rszFd, RSZ_S_EXP, &rszSpeed) == -1) {
+            ERR("Error calling RSZ_S_EXP on resizer.\n");
+            free(hRszcopy);
+            return RSZCOPY_FAILURE;
+        }
+    }
+
+    return hRszcopy;
+}
+
+/******************************************************************************
+ * Rszcopy_execute
+ ******************************************************************************/
+int Rszcopy_execute(Rszcopy_Handle hRszcopy, unsigned long srcBuf,
+                    unsigned long dstBuf)
+{
+    rsz_resize_t rsz;
+    int          rszError;
+
+    if (dstBuf % 32) {
+        ERR("Destination buffer not aligned on 32 bytes\n");
+        return RSZCOPY_FAILURE;
+    }
+
+    if (srcBuf % 32) {
+        ERR("Source buffer not aligned on 32 bytes\n");
+        return RSZCOPY_FAILURE;
+    }
+
+    rsz.in_buf.index     = -1;
+    rsz.in_buf.buf_type  = RSZ_BUF_IN;
+    rsz.in_buf.offset    = srcBuf;
+    rsz.in_buf.size      = hRszcopy->inSize;
+
+    rsz.out_buf.index    = -1;
+    rsz.out_buf.buf_type = RSZ_BUF_OUT;
+    rsz.out_buf.offset   = dstBuf;
+    rsz.out_buf.size     = hRszcopy->outSize;
+
+    do {
+        rszError = ioctl(hRszcopy->rszFd, RSZ_RESIZE, &rsz);
+    } while (rszError == -1 && errno == EAGAIN);
+
+    if (rszError == -1) {
+        ERR("Failed to execute resize job\n");
+        return RSZCOPY_FAILURE;
+    }
+
+    return RSZCOPY_SUCCESS;
+}
+
+/******************************************************************************
+ * Rszcopy_delete
+ ******************************************************************************/
+void Rszcopy_delete(Rszcopy_Handle hRszcopy)
+{
+    if (hRszcopy != NULL) {
+        close(hRszcopy->rszFd);
+        free(hRszcopy);
+    }
+}
+#endif //end of RESZ_HACK
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -33,6 +248,7 @@ unsigned int input = 0;
 typedef struct video_buffer_t
 {
 	void *start;
+	void * phys; // Let's query V4L2's physical address for RESZ DMA
 	int index;
 	ssize_t length;
 } video_buffer_t;
@@ -124,6 +340,9 @@ int video_reqbuf(video_t *v)
                                  PROT_READ | PROT_WRITE, /* recommended */
                                  MAP_SHARED,             /* recommended */
                                  v->fd, buffer.m.offset);
+
+		v->buffers[i].phys = buffer.m.offset;
+		//printf("V4L2 buffer physical address: %p\n", buffer.m.offset);
 
 		if (MAP_FAILED == v->buffers[i].start)
 			return -ENOMEM;
@@ -279,6 +498,19 @@ static int wait_4_vsync(int fd)
     return 0;
 }
 
+#if 0
+#include "sys/time.h"
+static unsigned int t1;
+static unsigned int get_time_ms(void)
+{
+	struct timeval tm;
+
+	gettimeofday(&tm, NULL);
+	tm.tv_sec %= 1000;
+	return (tm.tv_sec * 1000 + tm.tv_usec / 1000);
+}
+#endif
+
 int main(int argc, char **argv)
 {
 	video_t *v;
@@ -386,7 +618,10 @@ int main(int argc, char **argv)
 			break;
 		}
 
+		//t1 = get_time_ms();
 		wait_4_vsync(fb->fd);
+		//printf("delta = %u\n", get_time_ms() - t1);
+#ifndef RESZ_HACK
 		/* FIXME this is very harcoded, we need to check out the
  		 * format image and fb format */
 		/* send the captured image to the fb */
@@ -395,7 +630,43 @@ int main(int argc, char **argv)
 			memcpy((unsigned char *)fb->mmap + (fb->fix.line_length * i), \
 				   (unsigned char *)v->buffers[buffer.index].start + (format.fmt.pix.bytesperline * i), \
 				   format.fmt.pix.bytesperline);
+
 		}
+#else   //RESZ_HACK
+		{
+			if (RSZCOPY_FAILURE == hRszcopy)
+			{
+				/* Create the resize job */
+				hRszcopy = Rszcopy_create(RSZCOPY_DEFAULTRSZRATE);
+			}
+
+			if (hRszcopy == RSZCOPY_FAILURE)
+			{
+				ERR("Failed to create resize copy job\n");
+				goto err;
+			}
+
+			// FIXME: we do not actually resize, DMA only.
+			if (Rszcopy_config(hRszcopy,
+							   format.fmt.pix.bytesperline/2,
+							   lines,
+                               format.fmt.pix.bytesperline,
+							   format.fmt.pix.bytesperline) == RSZCOPY_FAILURE)
+			{
+				ERR("Failed to configure resize job\n");
+				goto err;
+			}
+
+			/* Copy frame to frame buffer using the H/W resizer */
+			if (Rszcopy_execute(hRszcopy, v->buffers[buffer.index].phys,
+								fb->phys) == RSZCOPY_FAILURE)
+			{
+				ERR("Frame copy using resizer failed\n");
+				goto err;
+			}
+		}
+#endif // end of RESZ_HACK
+
 		wait_4_vsync(fb->fd);
 
 		/* send the captured frame back to the queue */
@@ -412,6 +683,9 @@ int main(int argc, char **argv)
 	video_show(fb);
 	video_stop(v);
 err:
+#ifdef RESZ_HACK
+	if (RSZCOPY_FAILURE != hRszcopy) Rszcopy_delete(hRszcopy);
+#endif
 	video_close(v);
 	fb_delete(fb);
 	return 0;
